5252341dcffb5f407357512be40100ddb2145fe0
KevinMitnick <strong.darii@gmail.com>
2020-09-18 23:28:58 +0300

    2.17

--- Lab2/src/_2_17.java
+++ Lab2/src/_2_17.java
@@ -8,7 +8,6 @@ public class _2_17 {
         //shown in Fig. 2.15. [Note: The calculation of the average in this exercise should result in an integer
         //representation of the average. So, if the sum of the values is 7, the average should be 2, not
         //2.3333….]
-
         Scanner in = new Scanner(System.in);
         int[] arr = new int[3];
         System.out.print("Enter 1st val: ");

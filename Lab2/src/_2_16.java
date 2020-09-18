49ed8727fd066630fe3c58220545bd64788f44e5
KevinMitnick <strong.darii@gmail.com>
2020-09-18 23:28:31 +0300

    2.16

--- Lab2/src/_2_16.java
+++ Lab2/src/_2_16.java
@@ -2,9 +2,6 @@ import java.util.Scanner;
 
 public class _2_16 {
     public static void main(String[] args) {
-        //Write an application that asks the user to enter two integers, obtains
-        //them from the user and displays the larger number followed by the words "is larger". If the numbers are equal, print the message
-        // "These numbers are equal". Use the techniques shown in Fig. 2.15.
         Scanner in = new Scanner(System.in);
         System.out.print("Enter 1st val: ");
         int a = in.nextInt();

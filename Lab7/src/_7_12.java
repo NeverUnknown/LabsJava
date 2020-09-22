74563e4e7363c4087fe9a9a3be5bf37157181c13
KevinMitnick <strong.darii@gmail.com>
2020-09-22 14:37:45 +0300

    7.12 Duplicates

--- Lab7/src/_7_12.java
+++ Lab7/src/_7_12.java
@@ -5,7 +5,9 @@ public class _7_12 {
     public static void main(String[] args) {
         int[] arr = new int[5];
         Scanner in = new Scanner(System.in);
+        System.out.println("Enter 5 numbers:");
         for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter " + (i + 1) + " number>");
             arr[i] = in.nextInt();
         }
         in.close();
@@ -15,9 +17,9 @@ public class _7_12 {
         for (int i = 0; i < arr.length; i++) {
             if (i == 0) {
                 System.out.print(arr[i] + " ");
-            }
-            else if (arr[i] == prev) {
-                prev = arr[i];
+            } else if (arr[i] == prev) {
+                //prev = arr[i];
+                continue;
             } else {
                 System.out.print(arr[i] + " ");
                 prev = arr[i];

75e7f32508610aec84b3bea14cc377a4a23013f6
KevinMitnick <strong.darii@gmail.com>
2020-09-26 12:10:00 +0300

    7.12

--- Lab7/src/_7_12.java
+++ Lab7/src/_7_12.java
@@ -1,9 +1,10 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.sql.Struct;
+import java.util.*;
 
 public class _7_12 {
     public static void main(String[] args) {
         int[] arr = new int[5];
+
         Scanner in = new Scanner(System.in);
         System.out.println("Enter 5 numbers:");
         for (int i = 0; i < arr.length; i++) {
@@ -11,7 +12,7 @@ public class _7_12 {
             arr[i] = in.nextInt();
         }
         in.close();
-        Arrays.sort(arr);
+        /*Arrays.sort(arr);
         int prev = Arrays.stream(arr).findFirst().getAsInt();
         //System.out.print(prev + " ");
         for (int i = 0; i < arr.length; i++) {
@@ -24,6 +25,26 @@ public class _7_12 {
                 System.out.print(arr[i] + " ");
                 prev = arr[i];
             }
+        }*/
+        for (int i = 0, count = 0; i < arr.length; i++) {
+            boolean flag = false;
+            for (int j = 0; j < arr.length; j++) {
+                if (i == j && i != 4 && j != 4) {
+                    continue;
+                } else if (arr[i] == arr[j] && i != 4 && j != 4) {
+                    //flag = false;
+                    break;
+                } else if (j == arr.length - 1 && arr[i] != arr[j]) {
+                    //System.out.print(arr[i] + " ");
+                    flag = true;
+                }
+            }
+            if (flag) {
+                System.out.println(arr[i] + " ");
+            }
         }
+
+
     }
+
 }
---
+++ Lab8/src/SavingsAccountClass8_6.java
@@ -0,0 +1,8 @@
+public class SavingsAccountClass8_6 {
+
+}
+
+class SavingAccountClass {
+    static double annualInterestRate;
+    private double savingsBalance;
+}
